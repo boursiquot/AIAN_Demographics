059a46f3b19e1997bc4ad1d1507e873ac3b63427
BerniceBoursiquot <boursiquot@gmail.com>
2017-03-14 12:15:25 -0400

    Added new columns

--- aian_desc.py
+++ aian_desc.py
@@ -2,10 +2,26 @@ import pandas as pd
 
 aian_df = pd.read_csv("/Users/bernice/documents/ai_an/data/pums11_15_aian.csv", header = 0)
 
-#print aian_df.sample(5)
+# print aian_df.sample(5)
 
-print "Mean Age by AI/AN Alone or Combination", aian_df.groupby("ALONE")["AGEP"].mean()
+print "AI/AN Alone and in Combination", aian_df["ALONE"].value_counts()
+# print "Mean Age by AI/AN Alone or Combination", aian_df.groupby("ALONE")["AGEP"].mean()
 
-print "Top 5 States by AI/AN Alone or Combination", aian_df.groupby(["ALONE","ST"])["ST"].count().groupby(level = 0, group_keys = False).nlargest(5)
+# print "Top 5 States by AI/AN Alone or Combination", aian_df.groupby(["ALONE","ST"])["ST"].count().groupby(level = 0, group_keys = False).nlargest(5)
 
-print "Disability Status", aian_df.groupby()
+def counts (var1 , var2, label):
+	#print label, pd.crosstab(index = aian_df["ALONE"], columns = "var", margins = True )
+
+	print label, pd.crosstab(index = aian_df["ALONE"], columns = var1 , margins = True)	
+	print label, pd.crosstab(aian_df.ALONE, var2).apply(lambda r: (r/r.sum())*100, axis = 1)
+	
+
+counts (aian_df["DIS"], aian_df.DIS, "Disability Status")
+
+counts (aian_df["HINS4"], aian_df.PUBCOV, "Medicaid Status")
+
+counts (aian_df["PUBCOV"], aian_df.PUBCOV, "Public Health Insurance Status")
+
+counts (aian_df["PRIVCOV"], aian_df.PRIVCOV, "Private Health Insurance Status")
+
+counts (aian_df["HICOV"], aian_df.HICOV, "Private Health Insurance Status")
--- reading_pums.py
+++ reading_pums.py
@@ -17,19 +17,32 @@ start = dt.datetime.now().strftime("%Y/%m/%d %H:%M")
 
 print "Start Time", start 
 
-cols = ["serialno","SEX","AGEP","MAR","CIT","DIS","DEYE","DEAR","DDRS","DOUT","DPHY","DREM","HICOV","PRIVCOV","PUBCOV","HINS1","HINS2","HINS3","HINS4","HINS5","HINS6","HINS7","RAC1P","RAC2P05","RAC2P12","RACAIAN","RAC3P05","RAC3P12","HISP","ST","PUMA10","PWGTP"]
+cols = ["serialno","SEX","AGEP","MAR","CIT","DIS","DEYE","DEAR","DDRS","DOUT","DPHY","DREM","HICOV","PRIVCOV","PUBCOV","HINS1",
+"HINS2","HINS3","HINS4","HINS5","HINS6","HINS7","RAC1P","RAC2P05","RAC2P12","RAC3P05","RAC3P12","RACAIAN","RACBLK","RACASN",
+"RACNHPI","RACSOR","RACWHT","RACNUM","HISP","ST","PUMA00","PUMA10","PWGTP"]
 
 csvs = glob.glob("/Users/bernice/documents/ai_an/data/ss15*.csv")
 
 for i,f in enumerate(csvs):
-    tempdf = pd.read_csv(f,header = 0,index_col = "serialno", usecols = cols )
+    tempdf = pd.read_csv(f,header = 0,index_col = "serialno", usecols = cols, dtype ={
+	    	"AGEP": int,"SEX": int, "DOUT" : str, "serialno" : int,"MAR" : int,"CIT" : int,"DIS" : int,"DEYE" : int
+	        ,"DEAR" : int,"DDRS" : str,"DOUT" : str,"DPHY" : str,"DREM" : str,"HICOV" : int,"PRIVCOV" : int
+	        ,"PUBCOV" : int,"HINS1" : int, "HINS2" : int, "HINS3" : int,"HINS4" : int,"HINS5" : int,"HINS6" : int
+	        ,"HINS7" : int,"RAC1P" : int,"RAC2P05" : int,"RAC2P12" : int,"RACAIAN" : int,"RAC3P05" : int,"RAC3P12" : int
+	        ,"RACAIAN" : int, "RACBLK" : int, "RACASN" : int, "RACNHPI" : int, "RACSOR" : int, "RACWHT" : int, "RACNUM" : int
+	        , "HISP" : int, "ST" : int, "PUMA00" : str, "PUMA10" : str } )
     if i  == 0:
         acs11_15 = tempdf
     else:
         acs11_15 = acs11_15.append(tempdf)
 
 
+print acs11_15.head(5)
+
+
 # print "Number of observations", len(acs11_15.index)    
+
+print "Number of multirace people", acs11_15["RACNUM"].value_counts()
 
 # # Getting a count of the number of AI/AN alone or in combination
 
@@ -44,114 +57,122 @@ for i,f in enumerate(csvs):
 
 # Count is 312,208
 
-pums11_15_aian  = acs11_15[(acs11_15["RAC1P"] == 3)  | (acs11_15["RAC1P"] == 4) | (acs11_15["RAC1P"] == 5) | (acs11_15["RAC3P05"] == 34) | (acs11_15["RAC3P12"] == 3)   
-| (acs11_15["RAC3P05"] == 33) | (acs11_15["RAC3P05"] == 35) | (acs11_15["RAC3P05"] == 43) 
-| (acs11_15["RAC3P05"] == 47) | (acs11_15["RAC3P05"] == 64) | (acs11_15["RAC3P05"] == 65) | (acs11_15["RAC3P05"] == 67)
-| (acs11_15["RAC3P12"] == 17) | (acs11_15["RAC3P12"] == 30) | (acs11_15["RAC3P12"] == 39) | (acs11_15["RAC3P12"] == 40) 
-| (acs11_15["RAC3P12"] == 41) | (acs11_15["RAC3P12"] == 60) | (acs11_15["RAC3P12"] == 63) | (acs11_15["RAC3P12"] == 64) 
-| (acs11_15["RAC3P12"] == 79) | (acs11_15["RAC3P12"] == 81) | (acs11_15["RAC3P12"] == 82) | (acs11_15["RAC3P12"] == 84) 
-| (acs11_15["RAC3P12"] == 90) | (acs11_15["RAC3P12"] == 92) ] 
+pums11_15_aian  = acs11_15[(acs11_15["RACAIAN"] == 1)]
 
-# print "Number of people who are AI/AN", len(pums11_15_aian.index)
+# acs11_15[(acs11_15["RAC1P"] == 3)  | (acs11_15["RAC1P"] == 4) | (acs11_15["RAC1P"] == 5) | (acs11_15["RAC3P05"] == 34) | (acs11_15["RAC3P12"] == 3)   
+# | (acs11_15["RAC3P05"] == 33) | (acs11_15["RAC3P05"] == 35) | (acs11_15["RAC3P05"] == 43) 
+# | (acs11_15["RAC3P05"] == 47) | (acs11_15["RAC3P05"] == 64) | (acs11_15["RAC3P05"] == 65) | (acs11_15["RAC3P05"] == 67)
+# | (acs11_15["RAC3P12"] == 17) | (acs11_15["RAC3P12"] == 30) | (acs11_15["RAC3P12"] == 39) | (acs11_15["RAC3P12"] == 40) 
+# | (acs11_15["RAC3P12"] == 41) | (acs11_15["RAC3P12"] == 60) | (acs11_15["RAC3P12"] == 63) | (acs11_15["RAC3P12"] == 64) 
+# | (acs11_15["RAC3P12"] == 79) | (acs11_15["RAC3P12"] == 81) | (acs11_15["RAC3P12"] == 82) | (acs11_15["RAC3P12"] == 84) 
+# | (acs11_15["RAC3P12"] == 90) | (acs11_15["RAC3P12"] == 92) ] 
 
-
-def alonemulti(row):
-    if row["RAC1P"] == 3 or row["RAC1P"] == 4 or row["RAC1P"] == 5 or row["RAC3P05"] == 34 or row["RAC3P12"] == 3:
-        row["ALONE"] = 1
-
-    else:
-         row["ALONE"] = 0
-
-    return row
+print "Number of people who are AI/AN", len(pums11_15_aian.index)
 
 
+# def alonemulti(row):
+#     if row["RAC1P"] == 3 or row["RAC1P"] == 4 or row["RAC1P"] == 5 or row["RAC3P05"] == 34 or row["RAC3P12"] == 3:
+#         row["ALONE"] = 1
 
-# def hi_crosstab (var):
+#     else:
+#          row["ALONE"] = 0
 
-#     print pd.crosstab(index = df["HINS7"],  columns = var, margins = True)
-
-# hi_crosstab(df["HINS1"])
-# hi_crosstab(df["HINS2"])
-# hi_crosstab(df["HINS3"])
-# hi_crosstab(df["HINS4"])
-# hi_crosstab(df["HINS5"])
-# hi_crosstab(df["HINS6"])
+#     return row
 
 
-def health_insurance (row):
 
-    row["HEALTH_INSURANCE_COUNT"] = 0
-    row["IHS"]   = 0
-    row["IHS_ALONE"] = 0 
+# # def hi_crosstab (var):
 
-    if row["HINS1"] == 1 :
-        row["HEALTH_INSURANCE_COUNT"] = row["HEALTH_INSURANCE_COUNT"] + 1
+# #     print pd.crosstab(index = pums11_15_aian["HINS7"],  columns = var, margins = True)
+
+# # hi_crosstab(pums11_15_aian["HINS1"])
+# # hi_crosstab(pums11_15_aian["HINS2"])
+# # hi_crosstab(pums11_15_aian["HINS3"])
+# # hi_crosstab(pums11_15_aian["HINS4"])
+# # hi_crosstab(pums11_15_aian["HINS5"])
+# # hi_crosstab(pums11_15_aian["HINS6"])
+
+
+# def health_insurance (row):
+
+#     row["HEALTH_INSURANCE_COUNT"] = 0
+#     row["IHS"]   = 0
+#     row["IHS_ALONE"] = 0 
+
+#     if row["HINS1"] == 1 :
+#         row["HEALTH_INSURANCE_COUNT"] = row["HEALTH_INSURANCE_COUNT"] + 1
        
-    if row["HINS2"] == 1 :
-        row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
+#     if row["HINS2"] == 1 :
+#         row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
 
-    if row["HINS3"] == 1 :
-        row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
+#     if row["HINS3"] == 1 :
+#         row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
 
-    if row["HINS4"] == 1 :
-        row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
+#     if row["HINS4"] == 1 :
+#         row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
 
-    if row["HINS5"] == 1 :
-        row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
+#     if row["HINS5"] == 1 :
+#         row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
 
-    if row["HINS6"] == 1 :
-        row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
+#     if row["HINS6"] == 1 :
+#         row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
 
-    if row["HINS7"] == 1 :
-        row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
-        row["IHS"]   = 1
+#     if row["HINS7"] == 1 :
+#         row["HEALTH_INSURANCE_COUNT"] =  row["HEALTH_INSURANCE_COUNT"] + 1
+#         row["IHS"]   = 1
 
-    if row["HINS7"] == 1 and row["HINS1"] == 2  and row["HINS2"] == 2  and row["HINS3"] == 2  and row["HINS4"] == 2  and row["HINS5"] == 2 and row["HINS6"] == 2 :
-        row["IHS_ALONE"] = 1
+#     if row["HINS7"] == 1 and row["HINS1"] == 2  and row["HINS2"] == 2  and row["HINS3"] == 2  and row["HINS4"] == 2  and row["HINS5"] == 2 and row["HINS6"] == 2 :
+#         row["IHS_ALONE"] = 1
 
-    return row
-
-
-def disability (row):
-    row["DISABILITY_COUNT"] = 0
-
-    if row["DEYE"] == 1:
-        row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
-
-    if row["DEAR"] == 1:
-        row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
-
-    if row["DDRS"] == 1:
-        row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
-
-    if row["DOUT"] == 1:
-        row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
-
-    if row["DREM"] == 1:
-        row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
-
-    if row["DPHY"] == 1:
-        row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
-
-    return row
+#     return row
 
 
+# def disability (row):
+#     row["DISABILITY_COUNT"] = 0
 
-pums11_15_aian = pums11_15_aian.apply(alonemulti, axis = 1) ## axis = 0  apply function to each column, axis = 1 apply function to rows
+#     if row["DEYE"] == 1:
+#         row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
 
-pums11_15_aian = pums11_15_aian.apply(health_insurance, axis = 1) ## axis = 0  apply function to each column, axis = 1 apply function to rows
+#     if row["DEAR"] == 1:
+#         row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
 
-pums11_15_aian = pums11_15_aian.apply(disability, axis = 1) ## axis = 0  apply function to each column, axis = 1 apply function to rows
+#     if row["DDRS"] == 1:
+#         row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
 
-pums11_15_aian.to_csv("/Users/bernice/documents/ai_an/data/pums11_15_aian.csv")
+#     if row["DOUT"] == 1:
+#         row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
 
-print pums11_15_aian.sample(20)
+#     if row["DREM"] == 1:
+#         row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
+
+#     if row["DPHY"] == 1:
+#         row["DISABILITY_COUNT"] = row["DISABILITY_COUNT"] + 1
+
+#     return row
+
+
+
+# pums11_15_aian = pums11_15_aian.apply(alonemulti, axis = 1) ## axis = 0  apply function to each column, axis = 1 apply function to rows
+
+# print "ALONEMULTI function is complete"
+
+# pums11_15_aian = pums11_15_aian.apply(health_insurance, axis = 1) ## axis = 0  apply function to each column, axis = 1 apply function to rows
+
+# print "HEALTH_INSURANCE function is complete"
+
+# pums11_15_aian = pums11_15_aian.apply(disability, axis = 1) ## axis = 0  apply function to each column, axis = 1 apply function to rows
+
+# print "DISABILITY function is complete"
+
+# pums11_15_aian.to_csv("/Users/bernice/documents/ai_an/data/pums11_15_aian.csv")
+
+# print pums11_15_aian.sample(20)
 
 end = dt.datetime.now().strftime("%Y/%m/%d %H:%M")
 
-print "End time", end
 
-print "Time elapsed (sec) = {diff:1f}".format(diff = end - start)
+print "End time", end
+# print "Time elapsed (sec) = {diff:1f}".format(diff = end - start)
 
 
 
