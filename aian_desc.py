53bd5edb570f967328867445b3260f1cdd13812e
BerniceBoursiquot <boursiquot@gmail.com>
2017-03-15 22:27:11 -0400

    Added % for number of insurance plans

--- aian_desc.py
+++ aian_desc.py
@@ -7,13 +7,16 @@ aian_df = pd.read_csv("/Users/bernice/documents/ai_an/data/pums11_15_aian.csv", 
 print "AI/AN Alone and in Combination", aian_df["ALONE"].value_counts()
 # print "Mean Age by AI/AN Alone or Combination", aian_df.groupby("ALONE")["AGEP"].mean()
 
+print "Number of Health Insurance Plans", (aian_df["HEALTH_INSURANCE_COUNT"].value_counts(sort = False)/aian_df["HEALTH_INSURANCE_COUNT"].value_counts(sort = False).sum())*100
+
+
 # print "Top 5 States by AI/AN Alone or Combination", aian_df.groupby(["ALONE","ST"])["ST"].count().groupby(level = 0, group_keys = False).nlargest(5)
 
 def counts (var1 , var2, label):
 	#print label, pd.crosstab(index = aian_df["ALONE"], columns = "var", margins = True )
 
-	print label, pd.crosstab(index = aian_df["ALONE"], columns = var1 , margins = True)	
-	print label, pd.crosstab(aian_df.ALONE, var2).apply(lambda r: (r/r.sum())*100, axis = 1)
+	# print label, pd.crosstab(index = aian_df["ALONE"], columns = var1 , margins = True)	
+	print label, pd.crosstab(aian_df.ALONE, var2).apply(lambda r: (r/r.sum())*100.0, axis = 1)
 	
 
 counts (aian_df["DIS"], aian_df.DIS, "Disability Status")
@@ -25,3 +28,5 @@ counts (aian_df["PUBCOV"], aian_df.PUBCOV, "Public Health Insurance Status")
 counts (aian_df["PRIVCOV"], aian_df.PRIVCOV, "Private Health Insurance Status")
 
 counts (aian_df["HICOV"], aian_df.HICOV, "Health Insurance Status")
+
+counts (aian_df["HEALTH_INSURANCE_COUNT"], aian_df.HEALTH_INSURANCE_COUNT, "Number of Health Insurance Plans")
