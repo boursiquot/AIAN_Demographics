c99984f9c014987721b2014c16a577c7c49524e6
BerniceBoursiquot <boursiquot@gmail.com>
2017-06-20 23:41:45 -0400

    Added margin of error calculation

--- aian_desc.py
+++ aian_desc.py
@@ -1,5 +1,7 @@
+from __future__ import division
 import pandas as pd
 import numpy as np
+
 
 rep_pwgtp = []
 
@@ -19,13 +21,18 @@ alone_group = aian_df.groupby("ALONE")
 
 # print "AI/AN Total Population",aian_df["PWGTP"].sum()
 
-# # print "AI/AN Alone and in Combination (Unweighted)", aian_df["ALONE"].value_counts()
+# print "AI/AN Alone and in Combination (Unweighted)", aian_df["ALONE"].value_counts()
 
 # print "AI/AN Alone and in Combination (Weighted N)", alone_group["PWGTP"].sum()
 
 # print "AI/AN Alone and in Combination (Weighted %)",(alone_group["PWGTP"].sum()/aian_df["PWGTP"].sum())*100
 
 # print "Mean Age by AI/AN Alone or Combination (Weighted)", aian_df.groupby("ALONE")["AGEP"].mean()
+
+# print "IHS (Unweighted)", (aian_df["IHS_ALONE"].value_counts(sort = False)/aian_df["IHS_ALONE"].value_counts(sort = False).sum())*100
+
+# print "IHS (Weighted)", (aian_df.groupby("IHS_ALONE")["PWGTP"].sum()/aian_df["PWGTP"].sum())*100
+
 
 # print "Number of Health Insurance Plans (Unweighted)", (aian_df["HEALTH_INSURANCE_COUNT"].value_counts(sort = False)/aian_df["HEALTH_INSURANCE_COUNT"].value_counts(sort = False).sum())*100
 
@@ -38,70 +45,72 @@ alone_group = aian_df.groupby("ALONE")
 
 
 
-def counts ( var1, label):
-	#print label, pd.crosstab(index = aian_df["ALONE"], columns = "var", margins = True )
+def counts (var1, label):
 
-	# print label, pd.crosstab(index = aian_df["ALONE"], columns = var1 , margins = True)	
-	# var2 = (aian_df.groupby(["ALONE",var1])["PWGTP"].sum()/alone_group["PWGTP"].sum())*100.0
+	nsum = 0 
+	dsum = 0
 
-	# print label, (aian_df.groupby(["ALONE",var1])["PWGTP"].sum()/alone_group["PWGTP"].sum())*100.0
-	print label, aian_df.groupby(["ALONE",var1])["PWGTP"].sum()
+	j = 0
+
+	nbase = aian_df.groupby(["ALONE",var1])
+	cross = nbase["PWGTP"].sum()
+	print cross
+	x = list(cross)
+
+	while j < len(x):
+		for i in x:
+
 	
+			# nbase = aian_df[((aian_df["ALONE"] == 0) & (aian_df[var1] == 1))]
+			# dbase = aian_df[(aian_df["ALONE"] == 0)]
 
-	nbase = aian_df[((aian_df["ALONE"] == 0) & (aian_df[var1] == 1))]
-	dbase = aian_df[(aian_df["ALONE"] == 0)]
 
-	n0 = nbase["PWGTP"].sum()
-	d0 = dbase["PWGTP"].sum()
+			n0 = i
+			print "Current base estimate", n0
+			# d0 = dbase["PWGTP"].sum()
 
-	# n0 = aian_df[((aian_df["ALONE"] == 0) & (aian_df[var1] == 1))]["PWGTP"].sum()
-	# d0 = aian_df[(aian_df["ALONE"] == 0)]["PWGTP"].sum()
+			nsum = 0 
+			print "Reset nsum"
 
-	print n0, d0
-
-	nreps    = {}
-	dreps    = {}
-	nvalues  = []
-	dvalues  = []
+			for w in rep_pwgtp:
 	
+				n = list(nbase[w].sum())[j]  ## Applying weight
+				print "Weighted estimate", n
+				ndiff = np.square((n - n0))  ## Getting squared difference between [w] estimate and [0] estimat
 
-	nreps["n0"]  = n0
- 	dreps["n0"]  = d0
 
-	print nreps, dreps
+				# d = dbase[w].sum()  ## Applying weight
+				# ddiff = np.square((d - d0))  ## Getting squared difference between [w] estimate and [0] estimate
 
-	for w in rep_pwgtp:
+				nsum = nsum + ndiff 
+				print nsum
+
+				# Exit rep_pwgtp for loop 
+
+				# dsum = dsum + ddiff
+				
+
+			nse = np.sqrt((4/80) * nsum)
+		# dse = np.sqrt((4/80) * dsum)
+
+			nmoe = nse * 1.645
+
+			print "SE",nse, "Estimate", n0, "MOE", nmoe
+
+			j = j + 1
+
+			print j
+
+	# dmoe = dse * 1.645
+
+	# print label, pd.crosstab(index = aian_df["ALONE"], columns = aian_df[var1] , margins = True)	
 	
-		n = nbase[w].sum()
-		ndiff = np.square((n - n0))
-		nvalues.insert(0, n)
-		nvalues.insert(1, ndiff)
-
-
-		d = dbase[w].sum()
-		ddiff = np.square((d - d0))
-		dvalues.insert(0, d)
-		dvalues.insert(1, ddiff)
+	# print label, (aian_df.groupby(["ALONE",var1])["PWGTP"].sum()/alone_group["PWGTP"].sum()) * 100.0
+	# print label, aian_df.groupby(["ALONE",var1])["PWGTP"].sum()
 
 
 
-		nreps[w] = nvalues
-		dreps[w] = dvalues
-
-		nvalues = []
-		dvalues = []
-
-
-
-
-	print "Numerator replicates", nreps, "Denominator replicates", dreps
-
-
-
-	# print label, pd.crosstab(aian_df.ALONE, var2).apply(lambda r: (r/r.sum())*100, axis = 1)
-	
-
-counts ("DIS", "Disability Status (Weighted)")
+# counts ("DIS", "Disability Status (Weighted)")
 
 # counts ("HINS4", "Medicaid Status (Weighted)")
 
@@ -111,7 +120,7 @@ counts ("DIS", "Disability Status (Weighted)")
 
 # counts ("HICOV", "Health Insurance Status (Weighted)")
 
-# counts ("HEALTH_INSURANCE_COUNT",  "Number of Health Insurance Plans (Weighted)")
+counts ("HEALTH_INSURANCE_COUNT",  "Number of Health Insurance Plans (Weighted)")
 
 # counts ("IHS", "Number of People Who List IHS as Insurance (Weighted)" )
 
